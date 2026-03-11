7b74b57e17c3952e1b5a7d0bf80ac31a65ea4b15
Israel <israel.kugel10@gmail.com>
2026-03-11 03:06:50 +0200

    add remarks

--- app/src/main/java/com/example/demo2playersinfbrooms/FbModule.java
+++ app/src/main/java/com/example/demo2playersinfbrooms/FbModule.java
@@ -16,12 +16,6 @@ public class FbModule {
     //private GameStateListener listener;
     private Context context;
 
-    // Interface to pass game updates back to your UI
-/*    public interface GameStateListener {
-        void onGameUpdated(GameState state);
-        void onGameError(String error);
-    }*/
-
     public FbModule(String gameRoomId, String myPlayerId, Context context) {
         this.myPlayerId = myPlayerId;
         this.context = context;
@@ -42,7 +36,6 @@ public class FbModule {
 
             @Override
             public void onCancelled(DatabaseError error) {
-                //((GameActivity)context).onGameError(error.getMessage());
                 Toast.makeText(context, error.getMessage(), Toast.LENGTH_SHORT).show();
 
             }
@@ -52,7 +45,7 @@ public class FbModule {
      // update the Firebase with the new move, ensuring it is the player's turn.
 
     public void makeMove(Position position, boolean isWinningMove) {
-
+        // .get() This tells Firebase: "Go to the internet and fetch whatever data is currently sitting at gameRef."
         gameRef.get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
 
             @Override
@@ -88,36 +81,6 @@ public class FbModule {
                     } else {
                         Toast.makeText(context, "It's not your turn!", Toast.LENGTH_SHORT).show();
                     }
-                }
-            }
-        });
-    }
-    public void makeMove2(Position position) {
-        gameRef.get().addOnSuccessListener(snapshot -> {
-            GameState state = snapshot.getValue(GameState.class);
-
-            if (state != null && "PLAYING".equals(state.status))
-            {
-                // Ensure it's actually this player's turn
-                if (myPlayerId.equals(state.currentTurnId))
-                {
-                    state.lastMove = position;
-
-                    // Switch turns
-                    if (myPlayerId.equals(state.player1Id)) {
-                        // If I am Player 1, then it is now Player 2's turn
-                        state.currentTurnId = state.player2Id;
-                    } else {
-                        // Otherwise (I must be Player 2), so it is now Player 1's turn
-                        state.currentTurnId = state.player1Id;
-                    }
-
-                    // Push update to Firebase
-                    gameRef.setValue(state);
-                }
-                else
-                {
-                    Toast.makeText(context, "It's not your turn!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
